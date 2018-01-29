2c8a4494788d3b2ab2d1cfac1c70cf7e1754a6a0
Daniel Mota <daniel.mota@maua.br>
2018-01-29 11:50:43 -0200

    Initial

---
+++ FusedLocationProvider/MainActivity.cs
@@ -0,0 +1,289 @@
+using System;
+using Android.App;
+using Android.OS;
+using Android.Gms.Location;
+using Android.Gms.Common;
+using Android.Gms.Common.Apis;
+using Android.Util;
+using Android.Widget;
+using Android.Locations;
+using Android.Content;
+
+namespace FusedLocationProvider
+{
+	[Activity (Label = "FusedLocationProvider", MainLauncher = true)]
+	public class MainActivity : Activity, GoogleApiClient.IConnectionCallbacks,
+	    GoogleApiClient.IOnConnectionFailedListener, Android.Gms.Location.ILocationListener 
+	{
+		GoogleApiClient apiClient;
+		LocationRequest locRequest;
+		Button button;
+		TextView latitude;
+		TextView longitude;
+		TextView provider;
+		Button button2;
+		TextView latitude2;
+		TextView longitude2;
+		TextView provider2;
+        TextView txt_counter;
+        TextView txt_lastupdate;
+
+
+        int count = 0;
+
+        bool _isGooglePlayServicesInstalled;
+
+
+		////Lifecycle methods
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			Log.Debug ("OnCreate", "OnCreate called, initializing views...");
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// UI to print last location
+			button = FindViewById<Button> (Resource.Id.myButton);
+			latitude = FindViewById<TextView> (Resource.Id.latitude);
+			longitude = FindViewById<TextView> (Resource.Id.longitude);
+			provider = FindViewById<TextView> (Resource.Id.provider);
+
+			// UI to print location updates
+			button2 = FindViewById<Button> (Resource.Id.myButton2);
+			latitude2 = FindViewById<TextView> (Resource.Id.latitude2);
+			longitude2 = FindViewById<TextView> (Resource.Id.longitude2);
+			provider2 = FindViewById<TextView> (Resource.Id.provider2);
+
+            // UI to print status of system
+            txt_counter = FindViewById<TextView>(Resource.Id.txt_contador);
+            txt_lastupdate = FindViewById<TextView>(Resource.Id.txt_lastupdate);
+
+            _isGooglePlayServicesInstalled = IsGooglePlayServicesInstalled ();
+
+			if (_isGooglePlayServicesInstalled) {
+				// pass in the Context, ConnectionListener and ConnectionFailedListener
+				apiClient = new GoogleApiClient.Builder (this, this, this)
+					.AddApi (LocationServices.API).Build ();
+
+				// generate a location request that we will pass into a call for location updates
+				locRequest = new LocationRequest ();
+
+			} else {
+				Log.Error ("OnCreate", "Google Play Services is not installed");
+				Toast.MakeText (this, "Google Play Services is not installed", ToastLength.Long).Show ();
+				Finish ();
+			}
+
+		}
+
+		bool IsGooglePlayServicesInstalled()
+		{
+			int queryResult = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable (this);
+			if (queryResult == ConnectionResult.Success)
+			{
+				Log.Info ("MainActivity", "Google Play Services is installed on this device.");
+				return true;
+			}
+
+			if (GoogleApiAvailability.Instance.IsUserResolvableError (queryResult))
+			{
+				string errorString = GoogleApiAvailability.Instance.GetErrorString (queryResult);
+				Log.Error ("ManActivity", "There is a problem with Google Play Services on this device: {0} - {1}", queryResult, errorString);
+
+				// Show error dialog to let user debug google play services
+			}
+			return false;
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume ();
+			Log.Debug ("OnResume", "OnResume called, connecting to client...");
+
+			apiClient.Connect();
+
+			// Clicking the first button will make a one-time call to get the user's last location
+			button.Click += delegate {
+				if (apiClient.IsConnected)
+				{
+					button.Text = "Iniciar captura de dados";
+
+					Location location = LocationServices.FusedLocationApi.GetLastLocation (apiClient);
+					if (location != null)
+					{
+						latitude.Text = "Latitude: " + location.Latitude.ToString();
+						longitude.Text = "Longitude: " + location.Longitude.ToString();
+						provider.Text = "Provider: " + location.Provider.ToString();
+                        txt_counter.Text = "Number of observations: " + count.ToString();
+                        txt_lastupdate.Text = "Last observation: " + DateTime.Now.ToString();
+                        Log.Debug ("LocationClient", "Last location printed");
+                        SavetoSd(location.Latitude.ToString(), location.Longitude.ToString());
+                        CountDown();
+                        
+                    }
+				}
+				else
+				{
+					Log.Info ("LocationClient", "Please wait for client to connect");
+				}
+			};
+
+			// Clicking the second button will send a request for continuous updates
+			button2.Click += async delegate {
+				if (apiClient.IsConnected)
+				{
+					button2.Text = "Consultando posição inicial";
+
+					// Setting location priority to PRIORITY_HIGH_ACCURACY (100)
+					locRequest.SetPriority(100);
+
+					// Setting interval between updates, in milliseconds
+					// NOTE: the default FastestInterval is 1 minute. If you want to receive location updates more than 
+					// once a minute, you _must_ also change the FastestInterval to be less than or equal to your Interval
+					locRequest.SetFastestInterval(500);
+					locRequest.SetInterval(1000);
+
+					Log.Debug("LocationRequest", "Request priority set to status code {0}, interval set to {1} ms", 
+						locRequest.Priority.ToString(), locRequest.Interval.ToString());
+
+					// pass in a location request and LocationListener
+					await LocationServices.FusedLocationApi.RequestLocationUpdates (apiClient, locRequest, this);
+					// In OnLocationChanged (below), we will make calls to update the UI
+					// with the new location data
+				}
+				else
+				{
+					Log.Info("LocationClient", "Please wait for Client to connect");
+				}
+			};
+		}
+
+		protected override async void OnPause ()
+		{
+			base.OnPause ();
+			Log.Debug ("OnPause", "OnPause called, stopping location updates");
+
+			if (apiClient.IsConnected) {
+				// stop location updates, passing in the LocationListener
+				await LocationServices.FusedLocationApi.RemoveLocationUpdates (apiClient, this);
+
+				apiClient.Disconnect ();
+			}
+		}
+
+
+		////Interface methods
+
+		public void OnConnected (Bundle bundle)
+		{
+			// This method is called when we connect to the LocationClient. We can start location updated directly form
+			// here if desired, or we can do it in a lifecycle method, as shown above 
+
+			// You must implement this to implement the IGooglePlayServicesClientConnectionCallbacks Interface
+			Log.Info("LocationClient", "Now connected to client");
+		}
+
+		public void OnDisconnected ()
+		{
+			// This method is called when we disconnect from the LocationClient.
+
+			// You must implement this to implement the IGooglePlayServicesClientConnectionCallbacks Interface
+			Log.Info("LocationClient", "Now disconnected from client");
+		}
+
+		public void OnConnectionFailed (ConnectionResult bundle)
+		{
+			// This method is used to handle connection issues with the Google Play Services Client (LocationClient). 
+			// You can check if the connection has a resolution (bundle.HasResolution) and attempt to resolve it
+
+			// You must implement this to implement the IGooglePlayServicesClientOnConnectionFailedListener Interface
+			Log.Info("LocationClient", "Connection failed, attempting to reach google play services");
+		}
+
+		public void OnLocationChanged (Location location)
+		{
+			// This method returns changes in the user's location if they've been requested
+			 
+			// You must implement this to implement the Android.Gms.Locations.ILocationListener Interface
+			Log.Debug ("LocationClient", "Location updated");
+
+			latitude2.Text = "Latitude: " + location.Latitude.ToString();
+			longitude2.Text = "Longitude: " + location.Longitude.ToString();
+			provider2.Text = "Provider: " + location.Provider.ToString();
+            txt_counter.Text = "Number of observations: " + count.ToString();
+            txt_lastupdate.Text = "Last observation: " + DateTime.Now.ToString();
+        }
+
+		public void OnConnectionSuspended (int i)
+		{
+			
+		}
+
+        private void CountDown ()
+            {
+
+                System.Timers.Timer timer = new System.Timers.Timer();
+                timer.Interval = 1000; 
+                timer.Elapsed += OnTimedEvent;
+                timer.Enabled = true;
+
+            }
+
+            private void OnTimedEvent(object sender, System.Timers.ElapsedEventArgs e)
+            {
+                if (apiClient.IsConnected)
+                {
+                    button.Text = "Iniciar captura de dados";
+
+                    Location location = LocationServices.FusedLocationApi.GetLastLocation(apiClient);
+                    if (location != null)
+                    {
+                        latitude.Text = "Latitude: " + location.Latitude.ToString();
+                        longitude.Text = "Longitude: " + location.Longitude.ToString();
+                        provider.Text = "Provider: " + location.Provider.ToString();
+                        txt_counter.Text = "Number of observations: " + count.ToString();
+                        txt_lastupdate.Text = "Last observation: " + DateTime.Now.ToString();
+                        count++;
+                        Log.Debug("LocationClient", "Last location printed");
+                        SavetoSd(location.Latitude.ToString(), location.Longitude.ToString());
+                    }
+                }
+                else
+                {
+                    Log.Info("LocationClient", "Please wait for client to connect");
+                }
+
+        }
+
+        private void SavetoSd(String lat, String lon)
+        {
+            
+            var sdCardPath = Android.OS.Environment.ExternalStorageDirectory.Path;
+            var filePath = System.IO.Path.Combine(sdCardPath, "iootext.txt");
+            Log.Debug(filePath, "");
+
+            var filter = new IntentFilter(Intent.ActionBatteryChanged);
+            var battery = RegisterReceiver(null, filter);
+            int level = battery.GetIntExtra(BatteryManager.ExtraLevel, -1);
+            int scale = battery.GetIntExtra(BatteryManager.ExtraScale, -1);
+
+            int level_0_to_100 = (int)Math.Floor(level*100D/ scale);
+            Log.Debug("Battery Level: "+level_0_to_100.ToString(), "");
+
+
+            //           if (!System.IO.File.Exists(filePath))
+            //           {
+            using (System.IO.StreamWriter write = new System.IO.StreamWriter(filePath, true))
+                {
+                    write.Write(DateTime.Now.ToString()+","+level_0_to_100.ToString()+","+lat+","+lon+"\n");
+                }
+ //           }
+
+        }
+
+    }
+}
+
+
---
+++ FusedLocationProvider/obj/Release/android/src/md5933b3f9940c56b3291eb61c96278aefd/MainActivity.java
@@ -0,0 +1,104 @@
+package md5933b3f9940c56b3291eb61c96278aefd;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer,
+		com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks,
+		com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener,
+		com.google.android.gms.location.LocationListener
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onResume:()V:GetOnResumeHandler\n" +
+			"n_onPause:()V:GetOnPauseHandler\n" +
+			"n_onConnected:(Landroid/os/Bundle;)V:GetOnConnected_Landroid_os_Bundle_Handler:Android.Gms.Common.Apis.GoogleApiClient/IConnectionCallbacksInvoker, Xamarin.GooglePlayServices.Base\n" +
+			"n_onConnectionSuspended:(I)V:GetOnConnectionSuspended_IHandler:Android.Gms.Common.Apis.GoogleApiClient/IConnectionCallbacksInvoker, Xamarin.GooglePlayServices.Base\n" +
+			"n_onConnectionFailed:(Lcom/google/android/gms/common/ConnectionResult;)V:GetOnConnectionFailed_Lcom_google_android_gms_common_ConnectionResult_Handler:Android.Gms.Common.Apis.GoogleApiClient/IOnConnectionFailedListenerInvoker, Xamarin.GooglePlayServices.Base\n" +
+			"n_onLocationChanged:(Landroid/location/Location;)V:GetOnLocationChanged_Landroid_location_Location_Handler:Android.Gms.Location.ILocationListenerInvoker, Xamarin.GooglePlayServices.Location\n" +
+			"";
+		mono.android.Runtime.register ("FusedLocationProvider.MainActivity, FusedLocationProvider, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("FusedLocationProvider.MainActivity, FusedLocationProvider, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onResume ()
+	{
+		n_onResume ();
+	}
+
+	private native void n_onResume ();
+
+
+	public void onPause ()
+	{
+		n_onPause ();
+	}
+
+	private native void n_onPause ();
+
+
+	public void onConnected (android.os.Bundle p0)
+	{
+		n_onConnected (p0);
+	}
+
+	private native void n_onConnected (android.os.Bundle p0);
+
+
+	public void onConnectionSuspended (int p0)
+	{
+		n_onConnectionSuspended (p0);
+	}
+
+	private native void n_onConnectionSuspended (int p0);
+
+
+	public void onConnectionFailed (com.google.android.gms.common.ConnectionResult p0)
+	{
+		n_onConnectionFailed (p0);
+	}
+
+	private native void n_onConnectionFailed (com.google.android.gms.common.ConnectionResult p0);
+
+
+	public void onLocationChanged (android.location.Location p0)
+	{
+		n_onLocationChanged (p0);
+	}
+
+	private native void n_onLocationChanged (android.location.Location p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
